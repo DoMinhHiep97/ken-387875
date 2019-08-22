14749fa3f014fa3ac862379e686f8242355339ef
DoMinhHiep97 <54266147+DoMinhHiep97@users.noreply.github.com>
2019-08-22 13:46:47 +0700

    first commit

---
+++ exercisejs.js
@@ -0,0 +1,8 @@
+let inputWidth,inputHeight;
+inputWidth=prompt("Enteer the width");
+inputHeight=prompt( "Enter the height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=width*height;
+document.write("</br>");
+document.write("The are is: "+area);
